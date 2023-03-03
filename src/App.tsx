43ed7927365ffa4528e6e7c481130f66cdd72981
string_main <yhj2756@naver.com>
2023-03-03 21:55:16 +0900

    feat: 기본 구조 작성

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Header, SideBar } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SideBar = () => {
+  return (<div>사이드바</div>);
+};
+
+export default SideBar;
